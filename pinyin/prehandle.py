50d7d8aef4b00ca60ce589d4a235ec8ad5e741ef
fgj <fgj15@mails.tsinghua.edu.cn>
2019-03-29 21:09:10 +0800

    a b pi完善了，但发现直接用分词的结果还是不行呀

--- pinyin/prehandle.py
+++ pinyin/prehandle.py
@@ -1,8 +1,14 @@
 import json
-
+import pypinyin
+def listLoad(filename):
+    res=[]
+    with open(filename,'r') as f:
+        for l in f.readlines():
+            res.append(l.replace('\n',''))
+    return res
 
 def saveJson(filename, data):
-    jsObj = json.dumps(data,ensure_ascii=False)
+    jsObj = json.dumps(data,ensure_ascii=False, indent=4)
     with open(filename, 'w') as f:
         f.write(jsObj)
 
@@ -27,6 +33,56 @@ def readJson(filename):
     return data
 
 
+def bruteHandle(hanzi):
+    for h in hanzi:
+        if len(hanzi[h])>1:
+            num=len(hanzi[h])
+            for k in list(hanzi[h].keys()):
+                hanzi[h][k]=1/num
+
+
+def accurateHandle(hanzi):
+    # extract multi pinyin
+    multip=set()
+    for h in hanzi:
+        if len(hanzi[h])>1:
+            multip.add(h)
+    sens=listLoad('sentences.txt')
+    num=len(sens)
+    i=0
+    total=num
+    for s in sens[:total]:
+        i+=1
+        if i%100==0:
+            print("progress : %d / %d" %(i,total))
+        for w in s:
+            if w in multip:
+                try:
+                    pin=pypinyin.lazy_pinyin(s)
+                    index=s.index(w)
+                    p=pin[index]
+                    if p[-2:]=="ve":
+                        p=p.replace("ve","ue")
+                    if w=="哪" and p=="nei":
+                        p="na"
+                    elif w=="家" and p=="ji":
+                        p="jia"
+                    hanzi[w][p]+=1
+                    continue
+                except Exception as e:
+                    print("word : %s , pinyin : %s" %(w,p))
+                    print(s)
+                    print(pin)
+                
+
+    for w in multip:
+        s=sum(hanzi[w].values())
+        for k in hanzi[w]:
+            hanzi[w][k]/=s
+
+
+
+
 def emission():
     pinyin = readJson('pinyin.json')
     hanzi = {}
@@ -34,16 +90,13 @@ def emission():
         for h in pinyin[p]:
             hanzi.setdefault(h, {})
             hanzi[h][p] = 1
-    for h in hanzi:
-        if len(hanzi[h])>1:
-            num=len(hanzi[h])
-            for k in list(hanzi[h].keys()):
-                hanzi[h][k]=1/num
-    # print(hanzi)
+    # bruteHandle(hanzi)
+    accurateHandle(hanzi)
     saveJson("emission.json",hanzi)
 
 
 def main():
+    # savePinyin()
     emission()
 
 
